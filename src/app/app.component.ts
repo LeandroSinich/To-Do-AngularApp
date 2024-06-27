b14435d2d8389ad2cf8c9e56111d78c41fd9e7c5
LeandroSinich <leandrosinich@gmail.com>
2024-06-27 13:38:19 -0300

    base de todo app, servicio + html basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { TareasService } from './services/tareas.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'toDoAngularApp';
+export class AppComponent implements OnInit{
+  listaTareas: string[]= [];
+  nuevaTarea: string = '';
+
+  private _tareasService = inject(TareasService);
+
+  ngOnInit(): void {
+    this.listaTareas = this._tareasService.getTareas();
+    console.log(this.listaTareas);
+  }
+
+  agregarTarea(){
+    this._tareasService.setTarea(this.nuevaTarea);
+    this.nuevaTarea = '';
+    this.listaTareas = this._tareasService.getTareas();
+  }
+
+  eliminarTarea(index: number){
+    this._tareasService.eliminarTarea(index);
+    this.listaTareas = this._tareasService.getTareas();
+  }
 }
